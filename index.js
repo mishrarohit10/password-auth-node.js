67c61a07dd55dea4ed2b3722f8dd353b715d2b39
mishrarohit10 <mishrarohit316x@gmail.com>
2023-08-12 01:22:40 +0530

    finished

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bcrypt from "bcrypt";
 
 const app = express();
 
@@ -14,20 +15,39 @@ app.get("/users", (req, res) => {
 });
 
 app.post("/users", async(req, res) => {
-
   try {
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    console.log(salt);
-    console.log(hashedPassword);
-    const user = { name: req.body.name, password: req.body.hashedPassword };
+    // const salt = await bcrypt.genSalt();
+    // const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    // console.log(salt);
+    // console.log(hashedPassword);
+    const user = { name: req.body.name, password: hashedPassword };
     users.push(user); 
     res.status(201).send("user is added to the database");
   }
-  catch {
-    res.status(500).send();
+  catch(err){
+    console.log(err)
+    res.status(500).send("error");
   }
 });
+
+app.post("/users/login", async(req, res) => {
+  const user = users.find(user => user.name === req.body.name);
+  if (user == null){
+    return res.status(400).send("cannot find user");
+  }
+  try{
+    if(await bcrypt.compare(req.body.password, user.password)){
+      res.send("Success");
+    }
+    else{
+      res.send("Not Allowed");
+    }
+  }
+  catch{
+    res.status(500).send("error");
+  }
+})
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
